ee985ec08a7bc4fdb7d81bf4b839591704e15b3a
dani <danibarstad@gmail.com>
2021-02-24 20:16:22 -0600

    added function to test console output

--- scripts.js
+++ scripts.js
@@ -4,22 +4,46 @@ const games = document.getElementById("games");
 games.addEventListener("click", getGame)
 
 function getGame() {
-    fetch('https://zelda-api.apius.cc/api/games')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            appendData(data);
-        })
-        .catch(function (err) {
-            console.log('error: ' + err);
-        });
-    function appendData(data) {
-        var mainContainer = document.getElementById("title");
-        for (var i = 0; i <data.length; i++) {
-            var div = document.createElement("div");
-            div.innerHTML = "Title: " + data[i].name;
-            mainContainer.appendChild(div);
-        }
-    }
+    console.log("it worked!")
 }
+
+// function getGame() {
+//     fetch('https://zelda-api.apius.cc/api/games')
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             appendData(data);
+//         })
+//         .catch(function (err) {
+//             console.log('error: ' + err);
+//         });
+//     function appendData(data) {
+//         var mainContainer = document.getElementById("title");
+//         for (var i = 0; i <data.length; i++) {
+//             var div = document.createElement("div");
+//             div.innerHTML = "Title: " + data[i].name;
+//             mainContainer.appendChild(div);
+//         }
+//     }
+// }
+
+// async function getGame(url = '') {
+//     const response = await fetch(url, {
+//         method: 'GET',
+//         mode: 'no-cors',
+//         cache: 'no-cache',
+//         credentials: 'same-origin',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         redirect: 'follow',
+//         referrerPolicy: 'no-referrer'
+//     });
+//     return response.json();
+// }
+
+// getGame('https://zelda-api.apius.cc/api/games')
+//     .then(data => {
+//         console.log(data);
+//     })
