007e582beeb4a712aaa2a68a4187df2c533f7292
dani <danibarstad@gmail.com>
2021-02-24 22:21:04 -0600

    created and styled movie titles and descriptions

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
-const title = document.getElementById("title");
+// const title = document.getElementById("title");
 const games = document.getElementById("games");
+
+const main = document.getElementById('main');
+const container = document.createElement('div');
+container.setAttribute('class', 'container')
+main.appendChild(container);
 
 games.addEventListener("click", getGame)
 
@@ -19,12 +24,23 @@ function getGame() {
         .catch(function (err) {
             console.log('error: ' + err);
         });
-    function appendData(data) {
-        var mainContainer = document.getElementById("title");
-        for (var i = 0; i <data.length; i++) {
-            var div = document.createElement("div");
-            div.innerHTML = "Title: " + data[i].title;
-            mainContainer.appendChild(div);
+        function appendData(data) {
+            data.forEach(movie => {
+                const card = document.createElement('div');
+                card.setAttribute('class', 'card');
+    
+                const h1 = document.createElement('h1');
+                h1.setAttribute('id', 'title')
+                h1.textContent = movie.title;
+    
+                const p = document.createElement('p');
+                p.setAttribute('id', 'desc')
+                movie.description = movie.description.substring(0, 300);
+                p.textContent = `${movie.description}...`;
+    
+                container.appendChild(card);
+                card.appendChild(h1);
+                card.appendChild(p);
+            })
         }
-    }
 }
