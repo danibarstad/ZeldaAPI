37433c5b52436fa0fa4312b5e1cdc53c9bacf00a
dani <danibarstad@gmail.com>
2021-02-26 16:37:11 -0600

    commenting out server for now. CORS working with extension

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const http = require('http');
+// const http = require('http');
+// const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.end();
-});
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     fs.createReadStream('public\\index.html').pipe(res);
+//     res.end();
+// });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
