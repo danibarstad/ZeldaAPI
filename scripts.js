c82f44c30fdee2303b283ae6110573fc28ec8de7
dani <danibarstad@gmail.com>
2021-02-26 16:37:46 -0600

    successfully prints api data to console.

--- scripts.js
+++ scripts.js
@@ -14,20 +14,9 @@ games.addEventListener("click", getGame)
 
 function getGame() {
     // fetch('https://ghibliapi.herokuapp.com/films')
-
-
-    let myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
-    myHeaders.append('Access-Control-Allow-Origin', '*');
-    const myInit = {
-        method: 'GET', 
-        headers: myHeaders, 
-        mode: 'cors', 
-        cache: 'default'
-    };
-    fetch('https://zelda-api.apius.cc/api/games', myInit)
+    fetch('https://zelda-api.apius.cc/api/games')
         .then(function (response) {
-            return response;
+            return response.json();
         })
         .then(function(data) {
             // appendData(data);
@@ -36,23 +25,24 @@ function getGame() {
         .catch(function (err) {
             console.log('error: ' + err);
         });
-        // function appendData(data) {
-        //     data.forEach(movie => {
-        //         const card = document.createElement('div');
-        //         card.setAttribute('class', 'card');
+        function appendData(data) {
+            data = data['data'];
+            data.forEach(movie => {
+                const card = document.createElement('div');
+                card.setAttribute('class', 'card');
     
-        //         const h1 = document.createElement('h1');
-        //         h1.setAttribute('id', 'title')
-        //         h1.textContent = movie.title;
+                const h1 = document.createElement('h1');
+                h1.setAttribute('id', 'title')
+                h1.textContent = movie.title;
     
-        //         const p = document.createElement('p');
-        //         p.setAttribute('id', 'desc')
-        //         movie.description = movie.description.substring(0, 300);
-        //         p.textContent = `${movie.description}...`;
+                const p = document.createElement('p');
+                p.setAttribute('id', 'desc')
+                movie.description = movie.description.substring(0, 300);
+                p.textContent = `${movie.description}...`;
     
-        //         container.appendChild(card);
-        //         card.appendChild(h1);
-        //         card.appendChild(p);
-        //     })
-        // }
+                container.appendChild(card);
+                card.appendChild(h1);
+                card.appendChild(p);
+            })
+        }
 }
