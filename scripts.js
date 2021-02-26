9222ad8287d36d40bc90bda89c86b7c18bce8815
dani <danibarstad@gmail.com>
2021-02-25 22:56:49 -0600

    messing around with API calls some more

--- scripts.js
+++ scripts.js
@@ -13,34 +13,46 @@ games.addEventListener("click", getGame)
 // }
 
 function getGame() {
-    fetch('https://ghibliapi.herokuapp.com/films')
+    // fetch('https://ghibliapi.herokuapp.com/films')
+
+
+    let myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+    myHeaders.append('Access-Control-Allow-Origin', '*');
+    const myInit = {
+        method: 'GET', 
+        headers: myHeaders, 
+        mode: 'cors', 
+        cache: 'default'
+    };
+    fetch('https://zelda-api.apius.cc/api/games', myInit)
         .then(function (response) {
-            return response.json();
+            return response;
         })
         .then(function(data) {
-            appendData(data);
-            // console.log(data)
+            // appendData(data);
+            console.log(data);
         })
         .catch(function (err) {
             console.log('error: ' + err);
         });
-        function appendData(data) {
-            data.forEach(movie => {
-                const card = document.createElement('div');
-                card.setAttribute('class', 'card');
+        // function appendData(data) {
+        //     data.forEach(movie => {
+        //         const card = document.createElement('div');
+        //         card.setAttribute('class', 'card');
     
-                const h1 = document.createElement('h1');
-                h1.setAttribute('id', 'title')
-                h1.textContent = movie.title;
+        //         const h1 = document.createElement('h1');
+        //         h1.setAttribute('id', 'title')
+        //         h1.textContent = movie.title;
     
-                const p = document.createElement('p');
-                p.setAttribute('id', 'desc')
-                movie.description = movie.description.substring(0, 300);
-                p.textContent = `${movie.description}...`;
+        //         const p = document.createElement('p');
+        //         p.setAttribute('id', 'desc')
+        //         movie.description = movie.description.substring(0, 300);
+        //         p.textContent = `${movie.description}...`;
     
-                container.appendChild(card);
-                card.appendChild(h1);
-                card.appendChild(p);
-            })
-        }
+        //         container.appendChild(card);
+        //         card.appendChild(h1);
+        //         card.appendChild(p);
+        //     })
+        // }
 }
