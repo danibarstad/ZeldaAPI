f2e89594d665296ef6f7c2fec65737fc7913f0f1
dani <danibarstad@gmail.com>
2021-03-06 22:31:13 -0600

    creating express server

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
-// const http = require('http');
-// const fs = require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
 
-// const hostname = '127.0.0.1';
-// const port = 3000;
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/html');
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     fs.createReadStream('public\\index.html').pipe(res);
-//     res.end();
-// });
-
-// server.listen(port, hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-// });
+app.listen(3000);
